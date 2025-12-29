9074f7dac1a8bc504a8ce5ad9500cfba7dd4c783
michael-hoon <michaelhoon@gmail.com>
2025-12-29 22:32:40 +0800

    fix: remove import deprecation warning

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 import expressiveCode from 'astro-expressive-code';
 import mdx from '@astrojs/mdx';
